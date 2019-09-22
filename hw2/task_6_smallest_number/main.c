4fdb2d8833c3f70dfc78899e07caea9a53be5843
Utkin Ilya <utkin.ilya.nickolaevich@gmail.com>
2019-09-22 18:44:06 +0300

    speed up the program (from nlogn to n)

--- hw2/task_6_smallest_number/main.c
+++ hw2/task_6_smallest_number/main.c
@@ -1,72 +1,49 @@
 #include <stdio.h>
-#include <math.h>
-
-#define MAXSIZE 1000
-
-void quickSort(int left, int right, int array[])
-{
-    int arrayLength = right - left + 1;
-    int middle = array[arrayLength / 2 + left];
-    int leftPointer = left;
-    int rightPointer = right;
-
-    if (right < left)
-    {
-        return;
-    }
-
-    do
-    {
-        while (array[leftPointer] < middle)
-        {
-            leftPointer++;
-        }
-        while (array[rightPointer] > middle)
-        {
-            rightPointer--;
-        }
-        if (leftPointer <= rightPointer)
-        {
-            int temp = array[leftPointer];
-            array[leftPointer] = array[rightPointer];
-            array[rightPointer] = temp;
-            leftPointer++;
-            rightPointer--;
-        }
-    } while (leftPointer <= rightPointer);
-
-    quickSort(left, rightPointer , array);
-    quickSort(leftPointer, right, array);
-}
-
 
 int main()
 {
-    int numerals[MAXSIZE] = {0};
-    int numLength = 0;
     int number = 0;
 
     printf("Enter the number:\n");
     scanf("%d", &number);
 
+    int numerals[10] = {0};
+
     while (number > 0)
     {
-        numerals[numLength] = number % 10;
+        numerals[number % 10]++;
         number = number / 10;
-        numLength++;
     }
 
-    quickSort(0, numLength - 1, numerals);
-
     int result = 0;
-    int i = 0;
 
-    while (numLength > 0)
+    if (numerals[0] != 0)
     {
-        numLength--;
-        result += numerals[numLength] * pow(10, i);
-        i++;
+        int firstDigit = 1;
+        while (numerals[firstDigit] == 0)
+        {
+            firstDigit++;
+        }
+        numerals[firstDigit]--;
+        result += firstDigit;
+    }
+
+    int digitNumber = 0;
+
+    while (digitNumber != 9)
+    {
+        while ((numerals[digitNumber] == 0) && (digitNumber != 9))
+        {
+            digitNumber++;
+        }
+
+        while(numerals[digitNumber] != 0)
+        {
+            result = result * 10 + digitNumber;
+            numerals[digitNumber]--;
+        }
     }
 
     printf("Answer:\n%d", result);
+    return 0;
 }
