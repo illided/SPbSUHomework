98d9e340af643ae52f04ea28c8e479f957e53ad6
Utkin Ilya <utkin.ilya.nickolaevich@gmail.com>
2019-10-08 23:22:17 +0300

    Refactored the solution and added some commentaries

--- hw2/task_2_cows_and_bulls/main.c
+++ hw2/task_2_cows_and_bulls/main.c
@@ -2,87 +2,129 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define SHOW_THE_ANSWER false //change to true if you want the answer to be shown at the start of the game
+const bool showTheAnswer = true; //change to true if you want the answer to be shown at the start of the game
+const int maxSize = 80;
+
+int raiseToThePower(int number, int power)
+{
+    if (power == 0)
+    {
+        return 1;
+    }
+    if (power % 2 == 0)
+    {
+        int squareRootOfTargetNumber = raiseToThePower(number, power / 2);
+        return squareRootOfTargetNumber * squareRootOfTargetNumber;
+    }
+    return number * raiseToThePower(number, power - 1);
+}
+
+char* createString(int stringLength)
+{
+    char* localString = malloc(sizeof(char) * stringLength);
+    for (int i = 0; i < stringLength; i++)
+    {
+        localString[i] = ' ';
+    }
+    return localString;
+}
 
 int main() {
-    printf("Choose the difficulty (how many digits in the guessed number):\n");
+    printf("Choose the difficulty (how many digits in the guessed number less than 11):\n");
     int numOfDigits = 0;
     scanf("%d", &numOfDigits);
-
-    srand(time(NULL));
-    int *guessedNumber = malloc(numOfDigits * sizeof(int));
-    for (int i = 0; i < numOfDigits; i++)
+    if ((numOfDigits <= 0) || (numOfDigits > 10))
     {
-        guessedNumber[i] = 1 + rand() % 9;
+        printf("Incorrect input");
+        return 0;
     }
 
-    if (SHOW_THE_ANSWER)
+    // Creating the answer with non repetitive digits and converting it to string.
+    srand(time(NULL));
+    int *guessedNumberAsArrayOfInt = malloc(sizeof(int) * numOfDigits);
+    guessedNumberAsArrayOfInt[0] = 1 + rand() % 10;
+    // Setting the first digit to a non zero term
+    for (int i = 1; i < numOfDigits; i++)
     {
-        printf("Answer: ");
-        for (int m = 0; m < numOfDigits; m++)
+        int digit = 0;
+        // getting a non repetitive digit
+        bool isRepetitive = false;
+        do
         {
-            printf("%d", guessedNumber[m]);
+            isRepetitive = false;
+            digit = rand() % 10;
+            for (int j = 0; j < i; j++)
+            {
+                if (guessedNumberAsArrayOfInt[j] == digit)
+                {
+                    isRepetitive = true;
+                    break;
+                }
+            }
         }
-        printf("\n");
+        while (isRepetitive);
+        // and adding it to the array
+        guessedNumberAsArrayOfInt[i] = digit;
+    }
+
+    // converting the array of int to the array of char
+    char* guessedNumber = createString(maxSize);
+    for (int i = 0; i < numOfDigits; i++)
+    {
+        sprintf(&guessedNumber[i], "%d", guessedNumberAsArrayOfInt[i]);
+    }
+    // Deleting the array of int because it doesn't do anything now
+    free(guessedNumberAsArrayOfInt);
+    guessedNumber[numOfDigits] = '\0';
+
+
+    if (showTheAnswer)
+    {
+        printf("Hint: %s\n", guessedNumber);
     }
 
     int numOfTries = 0;
     int cows = 0;
     int bulls = 0;
-    int guessUnsplitted = 0;
-    printf("Start your guesses:\n");
+    char *guess = createString(maxSize);
     do
     {
-        numOfTries++;
         cows = 0;
         bulls = 0;
-        scanf("%d", &guessUnsplitted);
-        int spaceForGuess = numOfDigits;
-        int *guess = malloc(spaceForGuess * sizeof(int));
-        int guessLength = 0;
-        while (guessUnsplitted != 0)
+        scanf("%s", guess);
+        int guessLength = strlen(guess);
+        for (int guessCharPos = 0; guessCharPos < guessLength; guessCharPos++)
         {
-            if (guessLength == spaceForGuess)
-            {
-                spaceForGuess += 10;
-                guess = realloc(guess, spaceForGuess * sizeof(int));
-            }
-
-            guess[guessLength] = guessUnsplitted % 10;
-            guessUnsplitted = guessUnsplitted / 10;
-            guessLength++;
-        }
-
-        for (int k = 0; k < guessLength / 2; k++)
-        {
-            int temp = guess[k];
-            guess[k] = guess[guessLength - k - 1];
-            guess[guessLength - k - 1] = temp;
-        }
-
-        for (int digitPosition = 0; digitPosition < guessLength; digitPosition++)
-        {
-            if (guessedNumber[digitPosition] == guess[digitPosition])
+            if (guess[guessCharPos] == guessedNumber[guessCharPos])
             {
                 bulls++;
+                continue;
             }
-            else
+            for (int guessNumberCharPos = 0; guessNumberCharPos < numOfDigits; guessNumberCharPos++)
             {
-                for (int dgtPosInGuessedNumber = 0; dgtPosInGuessedNumber < numOfDigits; dgtPosInGuessedNumber++)
+                char charToCheck = guess[guessCharPos];
+                if (charToCheck == guessedNumber[guessNumberCharPos])
                 {
-                    if ((guessedNumber[dgtPosInGuessedNumber] == guess[digitPosition]) &&
-                            (guessedNumber[dgtPosInGuessedNumber] != guess[dgtPosInGuessedNumber]))
-                    {
-                        cows++;
-                    }
+                    cows++;
                 }
             }
-
         }
-        printf("%d cows and %d bulls\n", cows, bulls);
-        free(guess);
-    } while (bulls != numOfDigits);
+        printf("Bulls:%d Cows:%d\n", bulls, cows);
+        numOfTries++;
+
+        /*  Algorithm above says:
+        * Check for every digit in the guess:
+        * if on the same checked place the guessed number
+        * and assumption have the same digit then add one bull.
+        * Otherwise, check each element
+        * in the guessed number and if you find a match then add a cow. */
+    }
+    while (bulls != numOfDigits);  // and do it till guess == guessed number
     printf("You win!!! (you guessed %d times)", numOfTries);
+
+    free(guessedNumber);
+    free(guess);
     return 0;
 }
