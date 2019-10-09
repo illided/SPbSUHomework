3c9abb66c4f343f7688e004e8829f827c8ef325b
Utkin Ilya <utkin.ilya.nickolaevich@gmail.com>
2019-10-09 09:54:24 +0300

    highlighted the output as a separate function and added a little protection against incorrect input

--- hw2/task_2_all_the_fractions/main.c
+++ hw2/task_2_all_the_fractions/main.c
@@ -15,6 +15,18 @@ void addFraction(int numerator, int denominator, Fraction* fraction)
     fraction->denominator = denominator;
 }
 
+void printFractions(Fraction* arrayOfFractions, int arrayLength, int step)
+{
+    for (int i = 0; i < arrayLength; i++)
+    {
+        printf("%d/%d ", arrayOfFractions[i].numerator, arrayOfFractions[i].denominator);
+        if (i % step == 0)  // going to the next line every (step) fraction to add some readability;
+        {
+            printf("\n");
+        }
+    }
+}
+
 int main()
 {
     /* This solution uses a series of Faroese, in which each next fraction is
@@ -26,8 +38,13 @@ int main()
        q = round ((n + b) / d) * d - b   */
 
     int maxDenominator = 0;
-    printf("Enter the n:\n");
+    printf("Enter the n (n - is a natural number):\n");
     scanf("%d", &maxDenominator);
+    if (maxDenominator <= 0)
+    {
+        printf("incorrect input");
+        return 0;
+    }
 
     int arraySize = 5;
     Fraction* arrayOfFractions = malloc(sizeof(Fraction) * arraySize);  // creating the array of all fractions
@@ -37,11 +54,18 @@ int main()
     /* The first two fractions should be 0/1 and 1/n since they
     are obviously the smallest and the last will be 1/1. */
 
+    if (maxDenominator == 1)  // in case n = 1 answer will be 0/1 and 1/1
+    {
+        printf("All the simple fractions with the denominator less than %d:\n", maxDenominator);
+        printFractions(arrayOfFractions, 2, 5);
+        return 0;
+    }
+
     int arrayLength = 2;
     int numerator = 0;
     int denominator = 0;
 
-    do
+    do  // start creating the sequence
     {
         if (arrayLength == arraySize)  // adding more space if needed
         {
@@ -50,13 +74,13 @@ int main()
         }
         numerator = (maxDenominator + arrayOfFractions[arrayLength - 2].denominator) /
                 arrayOfFractions[arrayLength - 1].denominator * arrayOfFractions[arrayLength - 1].numerator -
-                arrayOfFractions[arrayLength - 2].numerator;
+                arrayOfFractions[arrayLength - 2].numerator;  // = p
 
         denominator = (maxDenominator + arrayOfFractions[arrayLength - 2].denominator) /
                 arrayOfFractions[arrayLength - 1].denominator * arrayOfFractions[arrayLength - 1].denominator -
-                arrayOfFractions[arrayLength - 2].denominator;
+                arrayOfFractions[arrayLength - 2].denominator;  // = q
 
-        addFraction(numerator, denominator, &arrayOfFractions[arrayLength]);
+        addFraction(numerator, denominator, &arrayOfFractions[arrayLength]);  // adding p/q
         arrayLength++;
     }
     while (arrayOfFractions[arrayLength - 1].denominator != 1);
@@ -65,14 +89,7 @@ int main()
      and there is no point to check the numerator of the last fraction*/
 
     printf("All the simple fractions with the denominator less than %d:\n", maxDenominator);
-    for (int i = 0; i < arrayLength; i++)
-    {
-        printf("%d/%d ", arrayOfFractions[i].numerator, arrayOfFractions[i].denominator);
-        if (i % 5 == 0)  // going to the next line every 5 fraction to add some readability;
-        {
-            printf("\n");
-        }
-    }
+    printFractions(arrayOfFractions, arrayLength, 5);
 
     free(arrayOfFractions);
     return 0;
