12bcebaafe6b7960de4805727ef32078400053aa
Utkin Ilya <utkin.ilya.nickolaevich@gmail.com>
2019-10-09 21:58:29 +0300

    Refactored solution entirely

--- hw2/task_4_many_maximums/main.c
+++ hw2/task_4_many_maximums/main.c
@@ -1,46 +1,82 @@
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
 
-const int maxSize = 10000;
+int *createArrayOfInt(int arrayLength)
+{
+    int *arrayOfInt = malloc(sizeof(int) * arrayLength);
+    for (int i = 0; i < arrayLength; i++)
+    {
+        arrayOfInt[i] = 0;
+    }
+    return arrayOfInt;
+}
+
+void quickSort(int left, int right, int array[])
+{
+    int arrayLength = right - left + 1;
+    int middle = array[arrayLength / 2 + left];
+    int leftPointer = left;
+    int rightPointer = right;
+
+    if (right < left)
+    {
+        return;
+    }
+
+    do
+    {
+        while (array[leftPointer] < middle)
+        {
+            leftPointer++;
+        }
+        while (array[rightPointer] > middle)
+        {
+            rightPointer--;
+        }
+        if (leftPointer <= rightPointer)
+        {
+            int temp = array[leftPointer];
+            array[leftPointer] = array[rightPointer];
+            array[rightPointer] = temp;
+            leftPointer++;
+            rightPointer--;
+        }
+    } while (leftPointer <= rightPointer);
+
+    quickSort(left, rightPointer , array);
+    quickSort(leftPointer, right, array);
+}
 
 int main() {
-    int array[maxSize];
-    for (int i = 0; i < maxSize; i++)
-    {
-        array[i] = 0;
-    }
-    int arrayLength = 0;
+    /* This solution is sorting the array
+     * and then looking for two
+     * equal numbers closest to the end.
+     * if such numbers are not found, then there
+     * are no numbers in the array that are repeated. */
 
-    printf("Enter the length of the array:\n");
+    printf("Enter the length of the array:\n");  // scanning the array
+    int arrayLength = 0;
     scanf("%d", &arrayLength);
     printf("Enter the array:\n");
-    for(int i = 0; i < arrayLength; i++)
+    int *array = createArrayOfInt(arrayLength);
+    for (int i = 0; i <arrayLength; i++)
     {
         scanf("%d", &array[i]);
     }
 
-    int singleMaximum = INT_MIN;
-    int multipleMaximum = INT_MIN;
+    quickSort(0, arrayLength - 1, array);  // sorting it
 
-    for (int i = 0; i < arrayLength; i++)
+    int pointer = arrayLength - 1;
+    for (; (array[pointer] != array[pointer - 1]) && (pointer > 1); pointer--);  // finding the biggest repetitive
+
+    if (pointer == 0)
     {
-        if (array[i] > singleMaximum)
-        {
-            singleMaximum = array[i];
-        }
-        else if (array[i] == singleMaximum)
-        {
-            multipleMaximum = singleMaximum;
-        }
+        printf("there are no numbers in the array that are repeated");
+        return 0;
     }
 
-    if (multipleMaximum != INT_MIN)
-    {
-        printf("Answer: %d", multipleMaximum);
-    }
-    else
-    {
-        printf("There is not a single maximum in the array that occurs more than once");
-    }
+    printf("The biggest repetitive number:\n%d", array[pointer]);
+
+    free(array);
     return 0;
 }
