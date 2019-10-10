367319460d090903e898d668702f21f4f1375a9b
Utkin Ilya <utkin.ilya.nickolaevich@gmail.com>
2019-10-10 21:21:03 +0300

    refactored data scan and added full solution

--- hw3/task_4_usersAndActions/main.c
+++ hw3/task_4_usersAndActions/main.c
@@ -2,28 +2,48 @@
 #include "simpleArrays.h"
 #include <stdlib.h>
 
+const int defaultSize = 80;
+
 int main() {
     printf("Enter the number of users, number of actions and time:\n");  // scanning the request
     int numOfUsers = 0;
-    int numOfActions = 0;
-    int time = 0;
-    scanf("%d %d %d", &numOfUsers, &numOfActions, &time);
+    int requestedNumOfActions = 0;
+    int requestedTime = 0;
+    scanf("%d %d %d", &numOfUsers, &requestedNumOfActions, &requestedTime);
 
-    int **users = malloc(sizeof(int*) * numOfUsers);  // creating the space for data and scanning it
+    /* Since we can check whether a user is suitable
+     * for a request or not without data about other users,
+     * we will only store data about one user at a time */
+
+    int *userActionsDates = createArrayOfInt(defaultSize);
     int numOfUserAction = 0;
+    int result = 0;
     for (int i = 0; i < numOfUsers; i++)
     {
-        printf("(User #%d) Enter the number of user actions:\n", i);
+        printf("(User #%d) Enter the number of user actions:\n", i);  // scanning the data for one user
         scanf("%d", &numOfUserAction);
-        users[i] = createArrayOfInt(numOfUserAction);
+        if (numOfUserAction > defaultSize)  // adding more space if necessary
+        {
+            userActionsDates = realloc(userActionsDates, sizeof(int) * numOfUserAction);
+        }
         printf("Enter user actions:\n");
-        scanArrayOfInt(0, numOfUserAction - 1, users[i]);
+        scanArrayOfInt(0, numOfUserAction - 1,userActionsDates);
+
+        int suitableActions = 0;  // check whether this user suits the request
+        for (int j = 0; j < numOfUserAction; j++)
+        {
+            if (userActionsDates[j] <= requestedTime)
+            {
+                suitableActions++;
+            }
+        }
+        if (suitableActions == requestedNumOfActions)
+        {
+            result++;
+        }
     }
 
-    for (int i = 0; i < numOfUsers; i++)  // deleting the data
-    {
-        free(users[i]);
-    }
-    free(users);
+    printf("Answer: %d", result);
+    free(userActionsDates);
     return 0;
 }
