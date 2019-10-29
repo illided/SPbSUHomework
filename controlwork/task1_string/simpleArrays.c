8c35a55e0b8e062abd9b6d4a2ab5b1b634b2f5e3
Utkin Ilya <utkin.ilya.nickolaevich@gmail.com>
2019-10-29 14:28:45 +0300

    created a solution for task1 in test 1

---
+++ controlwork/task1_string/simpleArrays.c
@@ -0,0 +1,49 @@
+#include "simpleArrays.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char* createString(int stringLength)
+{
+    char* localString = malloc(sizeof(char) * stringLength);
+    for (int i = 0; i < stringLength; i++)
+    {
+        localString[i] = ' ';
+    }
+    return localString;
+}
+
+unsigned char* createUnsignedString(int stringLength)
+{
+    unsigned char* localString = malloc(sizeof(unsigned char) * stringLength);
+    for (int i = 0; i < stringLength; i++)
+    {
+        localString[i] = ' ';
+    }
+    return localString;
+}
+
+int *createArrayOfInt(int arrayLength)
+{
+    int* arrayOfInt = malloc(sizeof(int) * arrayLength);
+    for (int i = 0; i < arrayLength; i++)
+    {
+        arrayOfInt[i] = 0;
+    }
+    return arrayOfInt;
+}
+
+void scanArrayOfInt(int begin, int end, int *arrayOfInt)
+{
+    for (int i = begin; i <= end; i++)
+    {
+        scanf("%d", &arrayOfInt[i]);
+    }
+}
+
+void printArrayOfInt(int begin, int end, int *arrayOfInt)
+{
+    for (int i = begin; i <= end; i++)
+    {
+        printf("%d", arrayOfInt[i]);
+    }
+}
